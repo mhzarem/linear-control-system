a439214ba03a7dbfd04dd5040de4073f5c4e0edd
Hossein Zare <mhzarem@gmail.com>
2018-10-21 09:56:25 +0330

    Add files via upload

--- section3.m
+++ section3.m
@@ -1,10 +1,35 @@
 % transient_response_analysis
 wn = 4 ; 
-damping_ratio = 0.2 ; 
+damping_ratio = 0.4 ; 
 [num0, den] = ord2(wn, damping_ratio);
 num = [2, 3];
 printsys(num,den,'s');
-t = 0:0.01:10;
-step(num, den,t);
+
+t = 0:0.01:20;
+y(:,1) = step(num, den,t);
 %text(2.2, 0.3,'salam')
-gtext('text')
+%gtext('text');
+num = [2 2 5];
+den = [2 2 7 4];
+y(:,2) = step(num,den,t);
+
+plot(t,y)
+
+p = 0;
+for i= 1:3
+    if p == 4 
+        p = 8 ;
+    end
+    p = p + 1;
+end  
+t = 0:0.1:50;
+a = [1 2 4 5];
+b = [1 8 7 6];
+z = zeros(501,4);
+for i = 1:4
+    num =[a(i)] ;
+    den = [1 b(i) a(i)];
+    z(:,i)= step(num, den,t);
+end 
+plot(t,z(:,1),'o','MarkerSize',1,'MarkerIndices',1:5:2501);
+grid on 
