21d77ed52ab3827d02b08f00872b689e89859cb5
Hossein Zare <mhzarem@gmail.com>
2018-10-21 11:02:19 +0330

    Add files via upload

--- impulse1.m
+++ impulse1.m
@@ -1,6 +1,10 @@
 t = 0:0.02:23;
-num = [2 5];
-den = [2 3 4];
+num_hard = [2 5];
+den_hard = [2 0.1 4];
+num_soft = [2 5];
+den_soft = [2 1 4];
+
+
 figure(1)
 y(:,1) = step(num,den,t);
 plot(y(:,1))
@@ -15,4 +19,21 @@ y(:,4)= heaviside(t);
 plot(t,y(:,1),t,y(:,4),t,-y(:,1)+1.5*y(:,4))
 legend('output','input','error')
 grid on
+
+figure(4)
+sys_soft = tf(num_soft,den_soft);
+y(:,5)= 0.2*sin(t)+ 0.05*cos(30*t);
+lsim(sys_soft,y(:,5),t)
+title('soft')
+grid on
+figure(5)
+sys_hard = tf(num_hard,den_hard);
+y(:,5)= 0.2*sin(t)+ 0.05*cos(30*t);
+lsim(sys_hard,y(:,5),t)
+title('hard')
+grid on
+figure(6)
+subplot(2,1,1),step(sys_hard),title('hard')
+subplot(2,1,2),step(sys_soft),title('soft')
+
 
